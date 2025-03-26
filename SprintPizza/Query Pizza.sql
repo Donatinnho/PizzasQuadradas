f9739763e5258ed432e07dd33fbe0f740985b05e
Donatinnho <lucas.donato@aluno.senai.br>
2025-03-26 15:03:29 -0300

    Tabela Pizza

---
+++ SprintPizza/Query Pizza.sql
@@ -0,0 +1,44 @@
+CREATE TABLE Cliente (
+id_cliente INTEGER PRIMARY KEY,
+telefone VARCHAR(15) NOT NULL,
+nome VARCHAR(30) NOT NULL,
+logradouro VARCHAR(30) NOT NULL,
+numero NUMERIC(5,0) NOT NULL,
+complemento VARCHAR(30),
+bairro VARCHAR(30) NOT NULL,
+cidade VARCHAR(30) NOT NULL,
+estado CHAR(2) NOT NULL,
+cep NUMERIC(8,0) NOT NULL,
+referencia VARCHAR(30)
+);
+
+SELECT * FROM Cliente;
+
+DROP TABLE Cliente;
+
+CREATE TABLE Pedidos (
+	id_pedido SERIAL PRIMARY KEY,
+    numero NUMERIC(5),
+    valor NUMERIC(6,2),
+    data TIMESTAMP,
+    telefone VARCHAR(15),
+    status VARCHAR(20),
+    forma_pagamento VARCHAR(20),
+    id_cliente INTEGER NOT NULL REFERENCES Cliente(id_cliente)
+);
+
+SELECT * FROM Pedidos;
+
+DROP TABLE Pedidos;
+
+CREATE TABLE Pizza (
+	id_pizza SERIAL PRIMARY KEY,
+	codigo NUMERIC(5,0) NOT NULL,
+	nome VARCHAR(30) NOT NULL,
+	descricao VARCHAR(100),
+	valor NUMERIC(5,2) NOT NULL
+);
+
+SELECT * FROM Pizza;
+
+DROP TABLE Pizza;
